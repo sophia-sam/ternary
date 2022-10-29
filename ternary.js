cdd84f70eed06466049ba14e6f6dacf3c10a3bc8
sophia-sam <sampsonjajasophia@gmail.com>
2022-10-29 11:27:40 +0100

    Update ternary.js
    applying ternary ,switch and if/else to get the same results.

--- ternary.js
+++ ternary.js
@@ -1,2 +1,24 @@
 const randomNumber = prompt("put in a number")
+          //Using ternary
 randomNumber < 10 ? console.log(`you have gorgeous eyes`) :randomNumber < 20 ? console.log(`you have gorgeous legs`) : console.log(`you are really smart` )
+           //OR using SWITCH
+switch (true) {
+    case  randomNumber < 10:
+        console.log(`you have gorgeous eyes`);
+        break;
+    case  randomNumber < 20:
+        console.log(`you have gorgeous legs`)
+         break;
+    default:console.log(`you are really smart`)
+        break;
+}
+          //OR using If/else
+          if (randomNumber < 10) {
+            console.log(`you have gorgeous eyes`)
+          }
+          else if (randomNumber < 20 ) {
+            console.log(`you have gorgeous legs`)
+          }
+          else{
+            console.log(`you are really smart`) 
+          }
